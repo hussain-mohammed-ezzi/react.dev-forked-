74e77ddb13c0a54496586ced98e9d461c2d52a67
hussain mohammed ezzi <109825958+hussain-mohammed-ezzi@users.noreply.github.com>
2024-01-28 11:53:03 +0000

    responding to the evenets

--- src/App.jsx
+++ src/App.jsx
@@ -1,78 +1,101 @@
-// // // here we are showing the functional
-// // import React from "react";
+// // // // here we are showing the functional
+// // // import React from "react";
 
-// // export function Item({ name, isPacked }) {
-// //   // all the props are provided in {}
-// //   return (
-// //     <div>
-// //       {name} {isPacked && "✔️"}
-// //     </div>
-// //   ); // all the rendering happens inside this , all the jsx elements
+// // // export function Item({ name, isPacked }) {
+// // //   // all the props are provided in {}
+// // //   return (
+// // //     <div>
+// // //       {name} {isPacked && "✔️"}
+// // //     </div>
+// // //   ); // all the rendering happens inside this , all the jsx elements
+// // // }
+
+// // // export default function Gallery() {
+// // //   return (
+// // //     <div>
+// // //       <h1>Shopping List</h1>
+// // //       <Item name="Apples" isPacked={true} />
+// // //       <Item name="Pomogrante" isPacked={false} />
+// // //       <Item name="Orange" isPacked={false} />
+// // //     </div>
+// // //   );
+// // // }
+
+// // ///making the pure functinos #####
+
+// // // what is not the pure funciton ##
+
+// // export function Guest({ number }) {
+// //   return <div>Guest number is {number}</div>;
 // // }
 
-// // export default function Gallery() {
+// // export default function GuestGallery() {
 // //   return (
 // //     <div>
-// //       <h1>Shopping List</h1>
-// //       <Item name="Apples" isPacked={true} />
-// //       <Item name="Pomogrante" isPacked={false} />
-// //       <Item name="Orange" isPacked={false} />
+// //       <h1>Guest List</h1>
+// //       <Guest number={1} />
+// //       <Guest number={2} />
+// //       <Guest number={3} />
 // //     </div>
 // //   );
 // // }
 
-// ///making the pure functinos #####
+// // making the button which will be used in the toolbar
 
-// // what is not the pure funciton ##
-
-// export function Guest({ number }) {
-//   return <div>Guest number is {number}</div>;
-// }
-
-// export default function GuestGallery() {
+// export function Button({ children, onClick }) {
 //   return (
 //     <div>
-//       <h1>Guest List</h1>
-//       <Guest number={1} />
-//       <Guest number={2} />
-//       <Guest number={3} />
+//       <button onClick={onClick}> {children}</button>
 //     </div>
 //   );
 // }
 
-// making the button which will be used in the toolbar
+// // making the tool bar which will have two buttoons
 
-export function Button({ children, onClick }) {
+// export function Toolbar({ uploadImage, notUploadmage }) {
+//   return (
+//     <div>
+//       <Button onClick={uploadImage}>click alert</Button>
+//       <Button onClick={notUploadmage}>not click alert</Button>
+//     </div>
+//   );
+// }
+
+// // making the componeet which will use the toolbar as a child
+// export default function ToolBarGalley() {
+//   return (
+//     <div>
+//       <Toolbar
+//         notUploadmage={() => {
+//           alert("this was not passed");
+//         }}
+//         uploadImage={() => {
+//           alert("this was pressed");
+//         }}
+//       />
+//     </div>
+//   );
+// }
+
+////Responding to the events page
+// making the button which component without any props
+import React from "react";
+export function Button() {
   return (
     <div>
-      <button onClick={onClick}> {children}</button>
+      <button>click</button>
     </div>
   );
 }
 
-// making the tool bar which will have two buttoons
-
-export function Toolbar({ uploadImage, notUploadmage }) {
+/// making a button with an handle
+export function Button() {
+  function handleClick() {
+    alert("clicked");
+  }
   return (
     <div>
-      <Button onClick={uploadImage}>click alert</Button>
-      <Button onClick={notUploadmage}>not click alert</Button>
-    </div>
-  );
-}
-
-// making the componeet which will use the toolbar as a child
-export default function Galley() {
-  return (
-    <div>
-      <Toolbar
-        notUploadmage={() => {
-          alert("this was not passed");
-        }}
-        uploadImage={() => {
-          alert("this was pressed");
-        }}
-      />
+      <button onClick={handleClick}>the alert button </button>
     </div>
   );
 }
