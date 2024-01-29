d3961e1848f4ecf0d344cf8441745cf4b45bc22f
hussain mohammed ezzi <109825958+hussain-mohammed-ezzi@users.noreply.github.com>
2024-01-29 16:53:34 +0000

    from the sani branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-// 
+//
 
-/// how will i make this onw 
+/// how will i make this onw
 /// what do you have to amek it
-// i have to make an app  
-// in app i will have the toolbar components which wil be passed by the app component all the props it needs 
-// the toolbar will use the Button componets which will accept the props by the toolbart 
-// the button element will use the deatult buttons , but it will pass the handlerevent porps to onclick 
+// i have to make an app
+// in app i will have the toolbar components which wil be passed by the app component all the props it needs
+// the toolbar will use the Button componets which will accept the props by the toolbart
+// the button element will use the deatult buttons , but it will pass the handlerevent porps to onclick
 
-// lets make the simplest of all the elemtns , the button one 
+// lets make the simplest of all the elemtns , the button one
 // export function Button({onClick ,childern}){
 //   return (
 //     <div>
@@ -28,51 +28,89 @@
 //   )
 // }
 
-// // now lets make the app element which uses the tool bar and passes it the props as inline functinos 
+// // now lets make the app element which uses the tool bar and passes it the props as inline functinos
 // export default function App (){
 //   return(
 //     <Toolbar onPlayMovieButton={()=>alert('you are playing a move , beware of fakhry bhai')}  onUploadMoveiButton={()=> alert('you are uploading the movei ,beware of it office ')}/>
 //   )
 // }
 
+// lets learn how to propogate the elements till the parents
+// but how will we do that
+// we will do that by making an element , which uses the button in enclosed div , but that div too will have some onlclick hadler attached to it
 
-// lets learn how to propogate the elements till the parents 
-// but how will we do that 
-// we will do that by making an element , which uses the button in enclosed div , but that div too will have some onlclick hadler attached to it 
+// now lets get back to the work
 
-// now lets get back to the work 
+// export default function HussainToolbar(){
+//   return(
+//     <div onClick={()=>{alert('this is an parent alert to make you laugh ')}}>
+//      <button onCLick={()=>alert('you aret playing hte child componeet')}>playing</button>
+//      <button onClick = {()=>alert('you are playing the child componeet 2')}>stopping</button>
+//     </div>
+//   )
+// }import React from 'react';
 
-export default function HussainToolbar(){
-  return(
-    <div onClick={()=>{alert('this is an parent alert to make you laugh ')}}>
-     <button onCLick={()=>alert('you aret playing hte child componeet')}>playing</button>
-     <button onClick = {()=>alert('you are playing the child componeet 2')}>stopping</button>
-    </div>
-  )
+// export function Button({ onClick, children }) {
+//   // The button is accepting the handle function as the props
+//   function handleClick(e) {
+//     e.stopPropagation();
+//     onClick();
+//   }
+//   return (
+//     <div>
+//       <button onClick={handleClick}>{children}</button>
+//     </div>
+//   );
+// }
+
+// // Now we will use this smart button in the HussainToolbar
+
+// export default function HussainToolbar() {
+//   return (
+//     <div>
+//       <Button onClick={() => alert("you are playing the child component")}>
+//         the first button
+//       </Button>
+//       <Button onClick={() => alert("you are playing the child component")}>
+//         the second button
+//       </Button>
+//     </div>
+//   );
+// }
+
+
+/// alhamdulliah , shall now we advance towards the state management 
+
+// now we will make an musem gallery , which feteches the data from the data file , and then shows the data as heading , the index of the picture and the image and some of the description about the picture 
+// what are you saing how ,will we do that 
+// its so easy get the data , we have go the data , now what to do , 
+// 
+import {sculptureList} from './data.js'
+import React from 'react'
+const data = sculptureList
+export default function Museaum(){
+// shall we first write the jsx or the java script , i think js will be writd 
+// no first the masooss then the maqool thats why the jsx firs 
+// now lets write the js which is not react perferct 
+
+//but what to write 
+// first make a variabel to store the index which we will increae with the handler funciton 
+var index = 1
+function IndexHandler(){
+  index = index +1+1
 }
+return(
+  <div>
 
-
-// now lets try to stop this propagation using e.stopPropgation
-// how will we do that , 
-// we will first make the button element which stops the propgations and then repeatedly used thatn 
-export function Button ({onClick}){ // hte button is accepting the handlefunction as the props 
-  function handleClick(e){
-    e.stopPropogation()
-    onClick()
-  }
-  return(
-    <div>
-    <button oncClick= { handleClick}></button>
-    </div>
-  )
+    <h1>Museaum Arts </h1>
+    <button onClick= {Inde}>Next</button>
+    <button onClick= {IndexHandler}> Next</button>
+    <h3>{data.index} out of {data.length}</h3>
+    <h2>the painting name {data.name}</h2>
+    <img scr = {data.url} alt = {data.url}>
+    <p>data.description</p>    
+    
+    
+  </div>
+)
 }
-
-// this button element can also be written as 
-export function Button ({onClick}){ // hte button is accepting the handlefunction as the props 
-  
-  return(
-    <div>
-    <button oncClick= { handlk}></button>
-    </div>
-  )
-
